d89b46a582085fb43e04a7c42c2bd1ca1d1e3444
Alexander Browne <alexlawford@icloud.com>
2024-02-25 08:48:04 +0800

    Updated to using controlnet inpainting pipeline

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import numpy as np
 
 cache_path = "./models"
 model_id = "runwayml/stable-diffusion-v1-5"
+controlnet_id = "lllyasviel/sd-controlnet-openpose"
 
 # The environment your app runs on
 app = App(
@@ -35,19 +36,26 @@ app = App(
 
 # Temp: load images for testing
 init_image = load_image(
+    "https://github.com/alexlawford/beam-sd-api/blob/c8351de9bace8e93bb6a6e4bc7a63c76b5053e24/example_init_image.png"
 );
 
 mask_image = load_image(
+    "https://github.com/alexlawford/beam-sd-api/blob/c8351de9bace8e93bb6a6e4bc7a63c76b5053e24/example_mask_image.png"
 );
 
 control_image = load_image(
+    "https://github.com/alexlawford/beam-sd-api/blob/c8351de9bace8e93bb6a6e4bc7a63c76b5053e24/example_control_image.png"
 );
 
 # This runs once when the container first boots
 def load_models():
-    pipe = StableDiffusionPipeline.from_pretrained(
+    controlnet = ControlNetModel.from_pretrained(
+        controlnet_id,
+        torch_dtype=torch.float16
+    )
+    pipe = StableDiffusionControlNetInpaintPipeline.from_pretrained(
         model_id,
-        revision="fp16",
+        controlnet=controlnet,
         torch_dtype=torch.float16,
         cache_dir=cache_path,
     ).to("cuda")
@@ -64,7 +72,7 @@ def generate_image(**inputs):
         prompt = inputs["prompt"]
     # Use a default prompt if none is provided
     except KeyError:
-        prompt = "a renaissance style photo of elon musk"
+        prompt = "A middle-aged man standing next to a polar bear in the artic"
     
     # Retrieve pre-loaded model from loader
     pipe = inputs["context"]
@@ -73,7 +81,14 @@ def generate_image(**inputs):
 
     with torch.inference_mode():
         with torch.autocast("cuda"):
-            image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+            image = pipe(
+                prompt,
+                num_inference_steps=50,
+                guidance_scale=7.5,
+                image=init_image,
+                mask_image=mask_image,
+                control_image=control_image
+    ).images[0]
 
     print(f"Saved Image: {image}")
     image.save("output.png")
